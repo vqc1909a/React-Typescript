bc74c8d7f8ff179ff41e11135a3f00a8f4060bde
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2025-07-16 21:36:53 -0500

    refactor: clean up Timer component code and improve comments

--- mi-app-fernando/src/components/Timer.tsx
+++ mi-app-fernando/src/components/Timer.tsx
@@ -1,6 +1,4 @@
 import {FC, useEffect, useState} from "react";
-
-
 interface TimerProps {
   milisegundos: number;
 }
@@ -8,9 +6,12 @@ export const Timer: FC<TimerProps> = ({milisegundos}) => {
 	const [segundos, setSegundos] = useState(0);
 
 	useEffect(() => {
-		const interval: number = setInterval(() => setSegundos((s) => s + 1), milisegundos);
+		const interval: number = setInterval(
+			() => setSegundos((s) => s + 1),
+			milisegundos
+		);
 
-		//It is executed when the component is unmounted or when the milisegundos value changes
+		//It's executed both when the component unmounts and before the effect re-runs because milisegundos changed.
 		return () => {
 			interval && clearInterval(interval);
 		};
